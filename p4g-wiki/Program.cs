a5ff747cbcf4dcd79afd3edd1442f2a8f78d001f
DeVoidDev <67163402+DeVoidDev@users.noreply.github.com>
2022-07-02 20:14:01 +0200

    Add HomeController

--- p4g-wiki/Program.cs
+++ p4g-wiki/Program.cs
@@ -4,8 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.ConfigureKestrel(option => option.AddServerHeader = false);
 builder.AddUnixSocket();
 builder.Services.AddRazorPages();
-Console.WriteLine(builder.Services);
 var app = builder.Build();
-app.MapGet("/", () => "Hello World!");
-
+app.MapControllers();
 app.Run();

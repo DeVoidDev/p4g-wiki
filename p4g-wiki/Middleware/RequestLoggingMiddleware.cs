c029dd2af01e4f127c366f9a3021b2ec739552e7
DeVoidDev <67163402+DeVoidDev@users.noreply.github.com>
2022-07-03 16:59:55 +0200

    Add Logging Middleware

---
+++ p4g-wiki/Middleware/RequestLoggingMiddleware.cs
@@ -0,0 +1,34 @@
+using System.Text;
+
+namespace P4GWiki.Middleware;
+
+public class RequestLoggingMiddleware
+{
+    private readonly ILogger<RequestLoggingMiddleware> _logger;
+    private readonly RequestDelegate _next;
+
+    public RequestLoggingMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+    {
+        _next = next;
+        _logger = loggerFactory.CreateLogger<RequestLoggingMiddleware>();
+    }
+    
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append("Received request:\n");
+        stringBuilder.Append("Headers:\n");
+        foreach (var (name, values) in context.Request.Headers)
+        {
+            foreach (var value in values)
+            {
+                stringBuilder.Append($"{name}: {value}\n");
+            }
+        }
+
+        stringBuilder.Append("Body:\n");
+        stringBuilder.Append(await new StreamReader(context.Request.Body).ReadToEndAsync());
+        _logger.LogInformation("{}", stringBuilder.ToString());
+        await _next(context);
+    }
+}

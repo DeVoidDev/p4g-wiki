cc855d8967bb0f4285f38e95cac4147efa6815d5
DeVoidDev <67163402+DeVoidDev@users.noreply.github.com>
2022-07-03 22:33:35 +0200

    Make Forwarded Header Case Insensitive

--- p4g-wiki/Middleware/RequestLoggingMiddleware.cs
+++ p4g-wiki/Middleware/RequestLoggingMiddleware.cs
@@ -12,19 +12,15 @@ public class RequestLoggingMiddleware
         _next = next;
         _logger = loggerFactory.CreateLogger<RequestLoggingMiddleware>();
     }
-    
+
     public async Task InvokeAsync(HttpContext context)
     {
         var stringBuilder = new StringBuilder();
         stringBuilder.Append("Received request:\n");
         stringBuilder.Append("Headers:\n");
         foreach (var (name, values) in context.Request.Headers)
-        {
-            foreach (var value in values)
-            {
-                stringBuilder.Append($"{name}: {value}\n");
-            }
-        }
+        foreach (var value in values)
+            stringBuilder.Append($"{name}: {value}\n");
 
         stringBuilder.Append("Body:\n");
         stringBuilder.Append(await new StreamReader(context.Request.Body).ReadToEndAsync());
